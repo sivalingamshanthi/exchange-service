8b99bea0ad4f2b4238566d3d7fd1bc960740a75f
siva <sshanthi@solstice.com>
2019-08-26 17:00:04 -0500

    Integration tests fixed

--- src/test/java/com/solstice/exchangeservice/ExchangeServiceIntegrationTest.java
+++ src/test/java/com/solstice/exchangeservice/ExchangeServiceIntegrationTest.java
@@ -7,12 +7,9 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
-import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.client.RestTemplate;
 
