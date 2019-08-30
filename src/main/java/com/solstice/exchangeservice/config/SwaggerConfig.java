3f519b709fea9e8de254641b080275efdf8adac2
siva <sshanthi@solstice.com>
2019-08-30 12:33:16 -0500

    Swagger Documentation Updated

--- src/main/java/com/solstice/exchangeservice/config/SwaggerConfig.java
+++ src/main/java/com/solstice/exchangeservice/config/SwaggerConfig.java
@@ -2,10 +2,8 @@ package com.solstice.exchangeservice.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.service.ApiInfo;
 import springfox.documentation.service.Tag;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
@@ -24,22 +22,3 @@ public class SwaggerConfig {
                 .tags(new Tag("Exchange Service Controller", "This is used for getting and posting Exchange Rates"));
     }
 }
-
-//@Configuration
-//@EnableSwagger2
-//public class SwaggerConfig {
-//
-//    @Bean
-//    public Docket productApi() {
-//        return new Docket(DocumentationType.SWAGGER_2).select()
-//                .apis(RequestHandlerSelectors.basePackage("my.package")).build()
-//                .apiInfo(apiInfo())
-//                .tags(new Tag("tag1", "Tag 1 description."),
-//                        new Tag("tag2", "Tag 2 description."),
-//                        new Tag("tag2", "Tag 3 description."));
-//    }
-//
-//    private ApiInfo apiInfo() {
-//        return new ApiInfoBuilder().title("My API").version("1.0.0").build();
-//    }
-//}
